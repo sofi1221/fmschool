97cdaf5f321034f9679f52f246375fb114aeed57
Sofi <sofi2421mail.ru@yandex.ru>
2020-04-21 13:35:04 +0500

    Add some functions, such as login and logout

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 def main():
-    pass
+    def load_user(user_id):
+        pass
+
+    def reqister():
+        pass
+
+    def login():
+        pass
+
+    def logout():
+        pass
 
 if __name__ == '__main__':
     main()
