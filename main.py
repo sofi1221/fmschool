a6bc050bf21f8b5f7bcb4c8e478ae641d7a434bf
Sofi <sofi2421mail.ru@yandex.ru>
2020-04-21 14:06:47 +0500

    Add head

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import flask
+from flask_login import LoginManager, login_user, login_required, logout_user
+from werkzeug.exceptions import abort
+from werkzeug.utils import redirect
+from flask import render_template, Flask, request
+import users_api
+
+from WEB.data import db_session
+from WEB.data.users import User, RegisterForm, LoginForm, InfoForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
 def main():
     def load_user(user_id):
         pass
